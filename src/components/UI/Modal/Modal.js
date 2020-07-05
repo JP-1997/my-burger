9d9d510a0b62a7545d70107b1131c7f62ff40973
JP-1997 <jyotiprakash.jpl.99@gmail.com>
2020-07-05 20:05:55 +0530

    added backdrop

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import classes from './Modal.module.css';
+import Auxiliary from '../../../hoc/Auxiliary';
+import Backdrop from '../Backdrop/Backdrop';
 
 const modal = (props) => (
-    <div
-        className={classes.Modal}
-        style={{
-            transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-            opacity: props.show ? '1' : '0'
-        }}>
+    <Auxiliary>
+        <Backdrop show={props.show} clicked={props.modalClosed} />
+        <div
+            className={classes.Modal}
+            style={{
+                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                opacity: props.show ? '1' : '0'
+            }}>
             {props.children}
-    </div>
+        </div>
+    </Auxiliary>
 );
 
 export default modal;
