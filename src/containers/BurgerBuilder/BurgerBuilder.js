ff471c2b77c2f01367ede80fc49bad03745514e5
JP-1997 <jyotiprakash.jpl.99@gmail.com>
2020-07-09 18:48:43 +0530

    storing order details on the server

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -77,7 +78,25 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You Continue!');
+       // alert('You Continue!');
+       const order = {
+           ingredients: this.state.ingredients,
+           price: this.state.totalPrice,
+           customer: {
+               name: 'JP',
+               address: {
+                   doorNumber: 'Test-123',
+                   street: 'Test Street 1',
+                   zipCode: '123456',
+                   country: 'India' 
+               },
+               email: 'test@test.com'
+           },
+           deliveryMethod: 'fastest'
+       }
+        axios.post('/orders.json', order)
+            .then(response => console.log(response))
+            .catch(error => console.log(error));
     }
 
     render() {
