9f8ed78052ad2e713ab45bdecdfbf3a42393fa66
JP-1997 <jyotiprakash.jpl.99@gmail.com>
2020-07-21 21:12:03 +0530

    storing the token on auth success

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
     token: null,
@@ -7,10 +8,33 @@ const initialState = {
     loading: false
 };
 
-const reducer = (state, action) => {
+const authStart = (state, action) => {
+    return updateObject(state, { error: null, loading: true });
+};
+
+const authSuccess = (state, action) => {
+    return updateObject(state, {
+        token: action.idToken,
+        userId: action.userId,
+        error: null,
+        loading: false
+    });
+};
+
+const authFail = (state, action) => {
+    return updateObject(state, {
+        error: action.error,
+        loading: false
+    });
+};
+
+const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case actionTypes.AUTH_START:
-            return {}
+        case actionTypes.AUTH_START: return authStart(state, action);
+        case actionTypes.AUTH_SUCCESS: return authSuccess(state, action);
+        case actionTypes.AUTH_FAIL: return authFail(state, action);
+        default:
+            return state;
     }
 };
 
